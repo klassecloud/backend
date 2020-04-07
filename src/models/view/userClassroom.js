ad4cd6a1a0a38b9e2af7406e2b655e42593fb352
Julius Schinschke <juliuss.16303@gmail.com>
2020-04-07 15:51:04 +0200

    Create view Models

---
+++ src/models/view/userClassroom.js
@@ -0,0 +1,12 @@
+
+export default class UserClassroom {
+  userClassroomId = undefined;
+
+  classroomName = undefined;
+
+  classroomTeacherName = undefined;
+
+  classroomPushKey = undefined;
+
+  classroomSubjects = undefined;
+}
---
+++ src/models/view/userSubjects.js
@@ -0,0 +1,7 @@
+export default class UserSubject {
+  subjectId = undefined;
+  subjectName = undefined;
+  subjectTeacherName = undefined;
+  subjectDescription = undefined;
+
+}
