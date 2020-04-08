f864b9d423ef4014c02d874378a4d7dd262dc290
Julius Schinschke <juliuss.16303@gmail.com>
2020-04-08 15:37:24 +0200

    Added update Task method

--- src/service/taskService.js
+++ src/service/taskService.js
@@ -39,6 +39,39 @@ export default class TaskService {
     return next();
   }
 
+  async updateTask(ctx, next) {
+    const { body } = ctx.request;
+    if (!ctx.user.isValidated) {
+      return ctx.throw(Boom.unauthorized('You are not a teacher or not validated'));
+    }
+    const task = await Task.findOne({ id: body.subjectId }, {  relations: ['files'] });
+    if (task === undefined) {
+      return ctx.throw(Boom.notFound('The given subject is not existing'));
+    }
+    const files = new Array();
+
+    body.files.forEach((taskfile) => {
+      const file = new File();
+      file.title = taskfile.title;
+      file.alt = taskfile.alt;
+      file.content = taskfile.content;
+      file.isCompressed = taskfile.isCompressed;
+      file.mimetype = taskfile.mimetype;
+      files.push(file);
+    });
+
+    task.title = body.title;
+    task.content = body.content;
+    task.startDate = body.startDate;
+    task.dueDate = body.dueDate;
+    task.files = files;
+
+    await task.save();
+    ctx.status = 201;
+
+    return next();
+  }
+
   async getSubjectTasks(ctx, next) {
     const subject = await Subject.findOne({id: ctx.params.subjectId}, {relations: ['tasks', 'tasks.files']});
     ctx.body = subject;
